cea58c46a6076432fb468c307d4e7ebd8cf5dacb
Bakhtiar Alam <alamnst2003@gmail.com>
2023-10-05 20:01:35 +0700

    front-end unigo

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const signUpButton = document.getElementById('signUp');
-const logInButton = document.getElementById('logIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-});
-
-logInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menuToggle');
     const nav = document.querySelector('nav');
