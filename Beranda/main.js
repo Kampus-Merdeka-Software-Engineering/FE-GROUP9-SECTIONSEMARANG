f4f70b31c21eb9b6999c3b5206adab9ad1c1b2d3
Bakhtiar Alam <alamnst2003@gmail.com>
2023-10-09 19:32:09 +0700

    frontend Unigo

--- Beranda/main.js
+++
@@ -1,9 +0,0 @@
-var navContainer = document.getElementById("navContainer");
-
-function showMenu() {
-  navContainer.style.right = "0";
-}
-
-function hideMenu() {
-  navContainer.style.right = "-200px";
-}
