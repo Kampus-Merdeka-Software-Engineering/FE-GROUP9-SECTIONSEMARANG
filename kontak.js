8600a2fb86e02b98f7ffeb50c449968fe630ac61
Bakhtiar Alam <alamnst2003@gmail.com>
2023-10-09 20:52:20 +0700

    Clone Repository

---
+++ kontak.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const nav = document.querySelector('nav');
+    const header = document.querySelector('header');
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        header.classList.toggle('active-header'); 
+        menuToggle.classList.toggle('active-menu'); 
+
+    });
+});
+
+
