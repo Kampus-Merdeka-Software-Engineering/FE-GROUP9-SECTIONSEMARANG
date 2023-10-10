32bc23517c3a89c8cc34d8cc686d5b7b565ea02f
made wahyu <imadewahyuartana@gmail.com>
2023-10-10 11:47:05 +0800

    conect to localhost mysql

--- kontak.js
+++ kontak.js
@@ -1,14 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menuToggle');
-    const nav = document.querySelector('nav');
-    const header = document.querySelector('header');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menuToggle");
+  const nav = document.querySelector("nav");
+  const header = document.querySelector("header");
 
-    menuToggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-        header.classList.toggle('active-header'); 
-        menuToggle.classList.toggle('active-menu'); 
-
-    });
+  menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    header.classList.toggle("active-header");
+    menuToggle.classList.toggle("active-menu");
+  });
 });
-
-
